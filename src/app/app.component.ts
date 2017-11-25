214789c2a4d584ca676d6367b2b264b18bc06374
manioo7 <bmaniprasad07@gmail.com>
2017-11-25 16:57:19 +0530

    heloo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'app mani!';
+clickMessage  = 'aasdas';
+
+	onclickMe(){
+	this.clickMessage  ="I am here ";
+
+	}
 }
+
