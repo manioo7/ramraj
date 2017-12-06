fef8cfa83e2e0a331c8f891c4f8faf1786d2235a
manioo7 <bmaniprasad07@gmail.com>
2017-12-07 00:27:36 +0530

    ngFor is add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import {BrowserModule} from '@angular/platform-browser'
 
 @Component({
   selector: 'app-root',
@@ -8,9 +8,13 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title = 'app mani!';
   public clickMessage  = '';
-
+ public names = [];
 	public onClickMe(){
 	  this.clickMessage  ="I am here ";
-	}
+	 
+   this.names = ['Angular2', 'Name2', 'name3'];
+ 
+	} 
+    
 }
 
